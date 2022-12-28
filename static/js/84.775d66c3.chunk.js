d979b3917cc4d8e8d133af5a5ead7e31dca6d061
Natha Tarkhala <Natha6515>
2022-12-28 22:39:11 +0530

    Updates

---
+++ static/js/84.775d66c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkraju_odedara=self.webpackChunkraju_odedara||[]).push([[84],{7084:function(e,a,r){r.r(a);r(2791),r(3508);var s=r(184);a.default=function(e){var a=e.title;return(0,s.jsx)("div",{className:"header",children:(0,s.jsx)("p",{className:"header_text",children:a})})}}}]);
+//# sourceMappingURL=84.775d66c3.chunk.js.map
